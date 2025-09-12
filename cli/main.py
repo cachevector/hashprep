ba31f3077a7f6ff841b5f3411e1ddf255ff18c68
maskedsyntax <aftaab@aftaab.xyz>
2025-09-13 02:01:56 +0530

    fix: update correct dataset path

--- cli/main.py
+++ cli/main.py
@@ -2,6 +2,6 @@ import pandas as pd
 from hashprep.analyzer import DatasetAnalyzer
 
 if __name__ == "__main__":
-    df = pd.read_csv("../datasets/train.csv")
+    df = pd.read_csv("datasets/train.csv")
     analyzer = DatasetAnalyzer(df)
     print(analyzer.analyze())
